683c571cdb0c0cf896100319f7df243d74cf880a
ryanmigulfakerwolf <54222910+ryanmigulfakerwolf@users.noreply.github.com>
2021-03-15 13:53:02 -0300

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,25 @@
+
+package Calculadora;
+
+public class Calculadora {
+    
+    double v1, v2;
+    double result;
+    
+    public double soma(double result){
+        result = v1+v2;
+        return result;
+    }
+    public double subtra(double result){
+        result = v1-v2;
+        return result;
+    }
+    public double multipli(double result){
+        result = v1*v2;
+        return result;
+    }
+    public double divisao(double result){
+        result = v1/v2;
+        return result;
+    }
+}
